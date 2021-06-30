c60f3c6663aef053e464a5f5e5d662804f0a17fd
VuKhanhSon <86724217+VuKhanhSon@users.noreply.github.com>
2021-06-30 20:00:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ const CONFIG = {
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Heh minh iu nhao nhaaaa <3',
+    mess: 'Anh biết mà. Yêu em nhiều nhiều <333',
+    messDesc: 'Heh minh iu nhao nhaaaa'
     btnAccept: 'Okiiiii lun <3',
 
